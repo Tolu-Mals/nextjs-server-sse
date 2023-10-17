f09810566ba20a1feb6ee192f39d46e7adf44f68
Tolulope Malomo <malomo.tolz@gmail.com>
2023-10-17 12:17:48 +0100

    server side sse consuming with POST

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,21 @@
-"use client";
-import { fetchEventSource } from "@microsoft/fetch-event-source";
-import { useState } from "react";
+import { fetchEventSource } from "@fortaine/fetch-event-source";
 
-const Page = () => {
-  const [message, setMessage] = useState("");
-
-  const handleClick = async () => {
-    setMessage("");
-    await fetchEventSource("/api", {
-      method: "POST",
-      onmessage(ev) {
-        setMessage((prev) => `${prev}${ev.data}\n`);
-      },
-    });
-  };
+const Page = async () => {
+  await fetchEventSource("http://localhost:3000/api", {
+    method: "POST",
+    async onmessage(ev) {
+      console.log("Message: ", ev.data);
+    },
+  });
 
   return (
     <div className="w-full p-4 max-w-md min-w-[200px]">
-      <button
-        onClick={handleClick}
-        className="bg-blue-500 px-4 py-2 rounded-md text-white"
-      >
+      <button className="bg-blue-500 px-4 py-2 rounded-md text-white">
         Start Stream
       </button>
 
       <div className="bg-white p-4 rounded-md mt-4 w-full whitespace-pre-line">
-        {message
-          ? message
-          : "Click on start stream button to see message generating here !"}
+        {"Click on start stream button to see message generating here !"}
       </div>
     </div>
   );
